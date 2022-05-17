f67d2328f4c555503c25ef42c0d40996f754d364
Russell LeVasseur <58618324+RussellLeVasseur@users.noreply.github.com>
2022-05-17 14:08:32 -0400

    Update OneDriveHealthCheck.ps1

--- OneDriveHealthCheck.ps1
+++ OneDriveHealthCheck.ps1
@@ -20,21 +20,29 @@ CreateStartUpShortcut;
 
 Try {
     If ($null -eq (Get-Process "onedrive" -ErrorAction SilentlyContinue)) {
-        Write-Host "Starting OneDrive...";
-        & $ODPath /background;
         Remove-Item -Path "$($env:USERPROFILE)\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\OneDrive.lnk";
         CreateStartUpShortcut;
+        Write-Host "Starting OneDrive...";
+        & "$($env:USERPROFILE)\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\OneDrive.lnk" /background;
     } Else { Write-Host "OneDrive is running."; }
 
     $Desktop = (Get-ItemProperty -path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -name "Desktop").Desktop;
     #$Pictures = (Get-ItemProperty -path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -name "My Pictures").'My Pictures';
     $Documents = (Get-ItemProperty -path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -name "Personal").Personal;
 
+    $ODLoggedIn = $false;
     If (Test-Path -Path "C:\ProgramData\Microsoft OneDrive\OneDriveStatus.txt") {
-        $ODCount = (Get-Content "C:\ProgramData\Microsoft OneDrive\OneDriveStatus.txt" | ConvertFrom-Json).Count;
-    } Else { $ODPath = 1; }
+        $ODUsers = (Get-Content "C:\ProgramData\Microsoft OneDrive\OneDriveStatus.txt" | ConvertFrom-Json).Value | Select-Object UserName;
+        ForEach ($User in $ODUsers) {
+            $Username = $User.Username.Split('\')[1];
+            If ($Username -eq $env:UserName){ $ODLoggedIn = $true; }
+        }
+    }
+    If ($ODCount -eq $false) { Write-Host "User does not appear to be logged in to OneDrive"; }
+    
 
-    If ($ODCount -lt 1 -OR $Desktop -notlike "$($env:OneDrive)*" -OR $Documents -notlike "$($env:OneDrive)*") {
+    If (($ODCount -eq $false) -OR ($Desktop -notlike "$($env:OneDrive)*") -OR ($Documents -notlike "$($env:OneDrive)*")) {
+        Write-Host "ClientEverSignedIn and SilentBusinessConfigCompleted being set to 0."
         Set-ItemProperty -Path Registry::HKCU\Software\Microsoft\OneDrive -Name ClientEverSignedIn -Value 0 -Type DWord;
         Set-ItemProperty -Path Registry::HKCU\Software\Microsoft\OneDrive -Name SilentBusinessConfigCompleted -Value 0 -Type DWord;
     }
